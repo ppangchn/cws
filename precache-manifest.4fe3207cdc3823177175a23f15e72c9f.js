a7da8e067f8cabda557151a3df7a6805f959cae2
Chanissa Trithipkaiwanpon <chanissat@Chanissas-MacBook-Pro.local>
2018-11-21 19:05:18 +0700

    Updates

---
+++ precache-manifest.4fe3207cdc3823177175a23f15e72c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5de063b6672e7f4e893",
+    "url": "/cws/static/js/runtime~main.c5de063b.js"
+  },
+  {
+    "revision": "8f8480cbeb64dcbaaf7a",
+    "url": "/cws/static/js/main.8f8480cb.chunk.js"
+  },
+  {
+    "revision": "acd256e038aed34f02c4",
+    "url": "/cws/static/js/1.acd256e0.chunk.js"
+  },
+  {
+    "revision": "8f8480cbeb64dcbaaf7a",
+    "url": "/cws/static/css/main.f47658b3.chunk.css"
+  },
+  {
+    "revision": "acd256e038aed34f02c4",
+    "url": "/cws/static/css/1.d742c3e5.chunk.css"
+  },
+  {
+    "revision": "7ab02582654c26ed9a5dde97d3712675",
+    "url": "/cws/index.html"
+  }
+];
